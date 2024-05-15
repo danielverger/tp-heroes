e30abb11bd097ec22860c933ac228b6e26c001d0
danielverger <daniel.verger@seera.sa>
2024-05-15 22:16:55 +0200

    Update docs

---
+++ docs/614.c44b2a9138f1122a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkw2m_front=self.webpackChunkw2m_front||[]).push([[614],{614:(t,h,n)=>{n.r(h),n.d(h,{DashboardRoutes:()=>s});const s=[{path:"",loadComponent:()=>Promise.all([n.e(98),n.e(226),n.e(819)]).then(n.bind(n,819)).then(o=>o.DashboardComponent),children:[{path:"heroes",loadChildren:()=>n.e(106).then(n.bind(n,6106)).then(o=>o.HeroesRoutes)}]}]}}]);
